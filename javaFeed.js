795b0d5488d9eb291ab222cd17b8c294cd7b101f
GabrielRASC5 <41081717+GabrielRASC5@users.noreply.github.com>
2018-08-16 10:14:16 -0400

    Update javaFeed.js

--- javaFeed.js
+++ javaFeed.js
@@ -1,14 +1,14 @@
-let account = localStorage.getItem('_account');
-if(account == null){
-  document.location.href ="index.html"
-}
-localStorage.removeItem('_account');
+// let account = localStorage.getItem('_account');
+// if(account == null){
+//   document.location.href ="index.html"
+// }
+// localStorage.removeItem('_account');
+// // console.log(account)
+// account = atob(account);
+// account = JSON.parse(account);
 // console.log(account)
-account = atob(account);
-account = JSON.parse(account);
-console.log(account)
 let account = {
-    NAME: "test",
+    NAME: "Luis",
     ID: 12345667
 }
 
