eef4f36337dba599cc162d6a48071e2213d5e90b
TonyASC5 <41081604+TonyASC5@users.noreply.github.com>
2018-08-16 11:00:32 -0400

    Add files via upload

---
+++ watch.js
@@ -0,0 +1,33 @@
+// let insta = document.getElementById("ligma");
+
+
+// function addPost(user){
+//     let mainContainer = document.createElement("div");
+//     mainContainer.id = "facebook"
+//     // let pElement = document.createElement("p");
+//     // pElement.id = "yeet";
+//     // pElement.innerText = profile.name;
+//     let imageElement = document.createElement("img");
+//     imageElement.id = "news";
+//     imageElement.href = "https://medium.com/all-star-code/forget-work-life-balance-its-all-about-work-life-harmony-a6841eeb4ad6";
+//     imageElement.src = "NewsArticle.jpeg";
+//     let proPic = document.createElement("img");
+//     proPic.id = "generic";
+//     proPic.src = "Profile_Pic.jpg";
+//     let profileElement = document.createElement("h2");
+//     profileElement.id = "luis";
+//     profileElement.innerHTML = user.name;
+//     let articleElement = document.createElement("a");
+//     articleElement.id = "article";
+//     articleElement.href = "https://medium.com/all-star-code/forget-work-life-balance-its-all-about-work-life-harmony-a6841eeb4ad6";
+//     articleElement.innerHTML = "Forget Work Life Balance, It's All About Work Life Harmony";
+//     mainContainer.appendChild(profileElement);
+//     mainContainer.appendChild(proPic);
+//     // mainContainer.appendChild(pElement);   
+//     mainContainer.appendChild(imageElement);
+//     mainContainer.appendChild(articleElement);
+//     insta.appendChild(mainContainer);
+
+// }  
+
+// addPost(user);
